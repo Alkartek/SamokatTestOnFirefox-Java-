2cb0e87bb831f8df06e837c5a146b1fefd6d16d1
Isakov Damir <alkartek@yandex.com>
2023-03-03 21:21:16 +0300

    TestJava

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+
+abstract public class BaseTest {
+    public void setUP(){
+        WebDriverManager.firefoxdriver().setup(); //выбираем и устанавливаем браузер
+        Configuration.browser = "firefox"; //называем бразуер
+        Configuration.driverManagerEnabled = true;
+        Configuration.browserSize = "3024x1964"; //Задаем размер экрана
+        Configuration.headless = false; // делаем браузер невидимым(тесты проходят без открытия браузера
+
+    }
+
+    @Before
+    public void init(){
+        setUP();
+    }
+
+    @After
+    public void tearDown(){
+        Selenide.closeWebDriver();
+    }
+}
